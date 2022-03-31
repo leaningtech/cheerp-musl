e3d71948c3b97a131732b45d7261a3f410dbfd3c
Yuri Iozzelli <y.iozzelli@gmail.com>
2022-03-31 15:33:59 +0200

    Add more atomic instructions

--- arch/cheerp/atomic_arch.h
+++ arch/cheerp/atomic_arch.h
@@ -1,33 +1,34 @@
 #define a_cas a_cas
 static inline int a_cas(volatile int *p, int t, int s)
 {
+	return __sync_val_compare_and_swap_4(p, t, s);
 //	__asm__ __volatile__ (
 //		"lock ; cmpxchg %3, %1"
 //		: "=a"(t), "=m"(*p) : "a"(t), "r"(s) : "memory" );
-	return t;
 }
 
 #define a_swap a_swap
 static inline int a_swap(volatile int *p, int v)
 {
+	return __sync_swap(p, v);
 //	__asm__ __volatile__(
 //		"xchg %0, %1"
 //		: "=r"(v), "=m"(*p) : "0"(v) : "memory" );
-	return v;
 }
 
 #define a_fetch_add a_fetch_add
 static inline int a_fetch_add(volatile int *p, int v)
 {
+	return __sync_fetch_and_add(p, v);
 //	__asm__ __volatile__(
 //		"lock ; xadd %0, %1"
 //		: "=r"(v), "=m"(*p) : "0"(v) : "memory" );
-	return v;
 }
 
 #define a_and a_and
 static inline void a_and(volatile int *p, int v)
 {
+	__sync_and_and_fetch(p, v);
 //	__asm__ __volatile__(
 //		"lock ; and %1, %0"
 //		: "=m"(*p) : "r"(v) : "memory" );
@@ -36,6 +37,7 @@ static inline void a_and(volatile int *p, int v)
 #define a_or a_or
 static inline void a_or(volatile int *p, int v)
 {
+	__sync_or_and_fetch(p, v);
 //	__asm__ __volatile__(
 //		"lock ; or %1, %0"
 //		: "=m"(*p) : "r"(v) : "memory" );
@@ -44,6 +46,7 @@ static inline void a_or(volatile int *p, int v)
 #define a_inc a_inc
 static inline void a_inc(volatile int *p)
 {
+	__sync_add_and_fetch(p, 1);
 //	__asm__ __volatile__(
 //		"lock ; incl %0"
 //		: "=m"(*p) : "m"(*p) : "memory" );
@@ -52,6 +55,7 @@ static inline void a_inc(volatile int *p)
 #define a_dec a_dec
 static inline void a_dec(volatile int *p)
 {
+	__sync_add_and_fetch(p, -1);
 //	__asm__ __volatile__(
 //		"lock ; decl %0"
 //		: "=m"(*p) : "m"(*p) : "memory" );
@@ -60,6 +64,7 @@ static inline void a_dec(volatile int *p)
 #define a_store a_store
 static inline void a_store(volatile int *p, int x)
 {
+	__atomic_store(p, x, __ATOMIC_SEQ_CST);
 //	__asm__ __volatile__(
 //		"mov %1, %0 ; lock ; orl $0,(%%esp)"
 //		: "=m"(*p) : "r"(x) : "memory" );
