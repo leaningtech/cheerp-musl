ed6eeae4a8675a06f20bf0eb4fa26360b435d049
Yuri Iozzelli <y.iozzelli@gmail.com>
2022-05-19 15:42:46 +0200

    Define off_t as unsigned int, and call __syscall instead of syscall for lseek

--- src/unistd/lseek.c
+++ src/unistd/lseek.c
@@ -5,9 +5,9 @@ off_t __lseek(int fd, off_t offset, int whence)
 {
 #ifdef SYS__llseek
 	off_t result;
-	return syscall(SYS__llseek, fd, offset>>32, offset, &result, whence) ? -1 : result;
+	return __syscall(SYS__llseek, fd, offset>>32, offset, &result, whence) ? -1 : result;
 #else
-	return syscall(SYS_lseek, fd, offset, whence);
+	return __syscall(SYS_lseek, fd, offset, whence);
 #endif
 }
 
