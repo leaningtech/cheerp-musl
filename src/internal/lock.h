4c7811e20bad5f2945837a845eaf5185bba8ce87
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-05-23 16:23:24 +0200

    LOCK/UNLOCK macro as no-ops

--- src/internal/lock.h
+++ src/internal/lock.h
@@ -3,7 +3,8 @@
 
 hidden void __lock(volatile int *);
 hidden void __unlock(volatile int *);
-#define LOCK(x) __lock(x)
-#define UNLOCK(x) __unlock(x)
+
+#define LOCK(x) (x)
+#define UNLOCK(x) (x)
 
 #endif
