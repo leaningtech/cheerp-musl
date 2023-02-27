5b74080abb937786e4f519b2534d36cf6b61a72c
Yuri Iozzelli <y.iozzelli@gmail.com>
2023-02-27 13:04:00 +0100

    Use brk in dlmalloc instead of mmap
    The reason is primarily a smaller size.

--- src/malloc/dlmalloc/malloc-params.h
+++ src/malloc/dlmalloc/malloc-params.h
@@ -1,7 +1,28 @@
-#define HAVE_MMAP 1
-#define HAVE_MORECORE 0
-#define UNSIGNED_MORECORE 1
+#define HAVE_MMAP 0
+#define HAVE_MORECORE 1
+#define MORECORE_CONTIGUOUS 1
 #define MORECORE_CANNOT_TRIM 1
+#define malloc_getpagesize (64*1024)
+#define DEFAULT_GRANULARITY malloc_getpagesize
+
+#include "syscall.h"
+
+__attribute__((cheerp_wasm))
+static inline void* __sbrk(int size)
+{
+	static char* end = 0;
+	if (!end)
+	{
+		end = (char*)SYS_brk(0);
+	}
+	char* ret = (char*)SYS_brk(end+size);
+	if (ret < end + size)
+		return (char*)-1;
+	char* base = end;
+	end += size;
+	return base;
+}
+#define MORECORE __sbrk
 
 #define ABORT __builtin_unreachable()
 #define NO_MALLOC_STATS 1
