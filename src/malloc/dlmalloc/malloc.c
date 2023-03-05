e638e413e72c2d4ac88f3ae681e72bf550cd3b59
Alessandro Pignotti <a.pignotti@sssup.it>
2023-03-05 11:50:21 +0100

    dlmalloc: Enable fast-paths on Cheerp

--- src/malloc/dlmalloc/malloc.c
+++ src/malloc/dlmalloc/malloc.c
@@ -2843,7 +2843,7 @@ static size_t traverse_and_check(mstate m);
 #define treebin_at(M,i)     (&((M)->treebins[i]))
 
 /* assign tree index for size S to variable I. Use x86 asm if possible  */
-#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__)) || defined(__CHEERP__)
 #define compute_tree_index(S, I)\
 {\
   unsigned int X = S >> TREEBIN_SHIFT;\
@@ -2946,7 +2946,7 @@ static size_t traverse_and_check(mstate m);
 
 /* index corresponding to given bit. Use x86 asm if possible */
 
-#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__)) || defined(__CHEERP__)
 #define compute_bit2idx(X, I)\
 {\
   unsigned int J;\
@@ -3181,7 +3181,7 @@ static int init_mparams(void) {
 #endif /* USE_DEV_RANDOM */
 #ifdef WIN32
       magic = (size_t)(GetTickCount() ^ (size_t)0x55555555U);
-#elif defined(LACKS_TIME_H)
+#elif defined(LACKS_TIME_H) || defined(__CHEERP__)
       magic = (size_t)&magic ^ (size_t)0x55555555U;
 #else
       magic = (size_t)(time(0) ^ (size_t)0x55555555U);
