39f1552a9e09d03dcc1cfcc3bb358dbff448d08f
Alessandro Pignotti <a.pignotti@sssup.it>
2023-03-07 08:48:47 +0100

    Fix conflicting declarations of allocation methods

--- include/malloc.h
+++ include/malloc.h
@@ -13,7 +13,13 @@ void *malloc (size_t);
 void *calloc (size_t, size_t);
 void *realloc (void *, size_t);
 void free (void *);
+#ifdef __CHEERP__
+__attribute((cheerp_asmjs))
+#endif
 void *valloc (size_t);
+#ifdef __CHEERP__
+__attribute((cheerp_asmjs))
+#endif
 void *memalign(size_t, size_t);
 
 size_t malloc_usable_size(void *);
