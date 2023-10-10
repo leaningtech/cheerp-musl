44bdcc6c92f86e5a1c6a019a2890ff7e3f096308
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-10-10 18:36:08 +0200

    make mbs?towwcs functions weak aliases

--- src/multibyte/mbstowcs.c
+++ src/multibyte/mbstowcs.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <wchar.h>
 
-size_t mbstowcs(wchar_t *restrict ws, const char *restrict s, size_t wn)
+size_t __mbstowcs(wchar_t *restrict ws, const char *restrict s, size_t wn)
 {
 	return mbsrtowcs(ws, (void*)&s, wn, 0);
 }
+
+__attribute__ ((__weak__, alias("__mbstowcs"))) size_t mbstowcs(wchar_t *restrict ws, const char *restrict s, size_t wn);
+__attribute__ ((alias("__mbstowcs"))) size_t __cheerp_mbstowcs(wchar_t *restrict ws, const char *restrict s, size_t wn);
