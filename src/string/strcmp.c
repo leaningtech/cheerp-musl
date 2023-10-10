0ac575d09db0e8d51e5b1ee2d272c7d43cea80bb
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-10-10 09:46:11 +0200

    make strcmp and strncmp weak aliases

--- src/string/strcmp.c
+++ src/string/strcmp.c
@@ -1,7 +1,10 @@
 #include <string.h>
 
-int strcmp(const char *l, const char *r)
+int __strcmp(const char *l, const char *r)
 {
 	for (; *l==*r && *l; l++, r++);
 	return *(unsigned char *)l - *(unsigned char *)r;
 }
+
+__attribute__ ((__weak__, alias("__strcmp"))) int strcmp(const char *l, const char *r);
+__attribute__ ((alias("__strcmp"))) int __cheerp_strcmp(const char *l, const char *r);
