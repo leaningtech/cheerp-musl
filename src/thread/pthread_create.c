823bc716777cae2901bb721af078755e5796ebdd
Mark Peerdeman <mark.peerdeman@leaningtech.com>
2024-06-18 15:18:27 +0200

    Close Worker threads when they are done
    Exit will not raise a signal if we're in a thread, but will do some important bookkeeping. So after calls to exit we close a worker properly.

--- src/thread/pthread_create.c
+++ src/thread/pthread_create.c
@@ -51,6 +51,12 @@ void __tl_sync(pthread_t td)
 	if (tl_lock_waiters) __wake(&__thread_list_lock, 1, 0);
 }
 
+__attribute__((cheerp_genericjs))
+void worker_close()
+{
+	__asm__("self.close()");
+}
+
 _Noreturn void __pthread_exit(void *result)
 {
 	pthread_t self = __pthread_self();
@@ -103,6 +109,7 @@ _Noreturn void __pthread_exit(void *result)
 		self->detach_state = state;
 		__restore_sigs(&set);
 		exit(0);
+		worker_close();
 	}
 
 	/* At this point we are committed to thread termination. */
@@ -165,7 +172,8 @@ _Noreturn void __pthread_exit(void *result)
 	self->tid = 0;
 	UNLOCK(self->killlock);
 
-	for (;;) __syscall(SYS_exit, 0);
+	__syscall(SYS_exit, 0);
+	worker_close();
 }
 
 void __do_cleanup_push(struct __ptcb *cb)
