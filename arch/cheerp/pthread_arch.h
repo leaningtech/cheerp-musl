59fef789428849fde19849f9b737e8afcaac4112
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-09-13 16:22:17 +0200

    fix: don't call __builtin_cheerp_get_thread_pointer on the genericjs target

--- arch/cheerp/pthread_arch.h
+++ arch/cheerp/pthread_arch.h
@@ -7,7 +7,11 @@ struct __pthread;
 extern struct __pthread __dummy_thread;
 static inline struct __pthread* __get_tp()
 {
+#ifdef __ASMJS__
 	return (struct __pthread*)__builtin_cheerp_get_thread_pointer();
+#else
+	return &__dummy_thread;
+#endif
 }
 
 #define MC_PC gregs[REG_EIP]
