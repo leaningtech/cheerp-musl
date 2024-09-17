6b0d5086c10c2911b81dba7d4a4d112b46888dcd
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-09-17 17:02:53 +0200

    Return __dummy_thread from __builtin_cheerp_get_thread_pointer when lowering atomics
    when we lower the builtin returns 0, so we check that and return the
    dummy thread global, as for genericjs.

--- arch/cheerp/pthread_arch.h
+++ arch/cheerp/pthread_arch.h
@@ -8,7 +8,8 @@ extern struct __pthread __dummy_thread;
 static inline struct __pthread* __get_tp()
 {
 #ifdef __ASMJS__
-	return (struct __pthread*)__builtin_cheerp_get_thread_pointer();
+	struct __pthread* ret = (struct __pthread*)__builtin_cheerp_get_thread_pointer();
+	return ret == 0? &__dummy_thread : ret;
 #else
 	return &__dummy_thread;
 #endif
