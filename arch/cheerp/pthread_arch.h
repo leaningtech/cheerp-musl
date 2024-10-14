f78b6e3b0b9d9109cbdaaeb93072e867af42d7e8
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-10-14 15:24:45 +0200

    remove unnecessary declaration of builtin

--- arch/cheerp/pthread_arch.h
+++ arch/cheerp/pthread_arch.h
@@ -1,8 +1,6 @@
 #ifndef __CHEERP_PTHREAD_ARCH_H
 #define __CHEERP_PTHREAD_ARCH_H
 
-unsigned int __builtin_cheerp_get_thread_pointer();
-
 struct __pthread; 
 extern struct __pthread __dummy_thread;
 static inline struct __pthread* __get_tp()
